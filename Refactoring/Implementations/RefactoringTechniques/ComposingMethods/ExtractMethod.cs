ea2a0eccb17cc6b98433de6fe182ee3cfd7bb045
Sam Park <SamParkLCF@hotmail.com>
2024-03-17 07:04:43 +0000

    Add implementation for "ExtractMethod" refactoring technique.

---
+++ Refactoring/Implementations/RefactoringTechniques/ComposingMethods/ExtractMethod.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SysAlchemy.Lab.Refactoring.Implementations.RefactoringTechniques.ComposingMethods;
+
+/// <summary>
+/// This class is an implementation of
+/// <see cref="SysAlchemy.Lab.Refactoring.Definitions.RefactoringTechniques.ComposingMethods.ExtractMethod"/>
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Local")]
+public class ExtractMethod
+{
+    private void PrintBanner()
+    {
+        // ...    
+    }
+
+    private string GetOutstanding()
+    {
+        // ...
+
+        return string.Empty;
+    }
+
+    private void PrintOwing_BeforeRefactoring(string name)
+    {
+        PrintBanner();
+
+        // Print details.
+        Console.WriteLine("name: " + name);
+        Console.WriteLine("amount: " + GetOutstanding());
+    }
+
+    // In 'Print details' section of the 'PrintOwing' method, we can extract the codes into a new method:
+    private void PrintDetails(string name)
+    {
+        Console.WriteLine("name: " + name);
+        Console.WriteLine("amount: " + GetOutstanding());
+    }
+
+    // After refactoring:
+    private void PrintOwing_AfterRefactoring(string name)
+    {
+        PrintBanner();
+        PrintDetails(name);
+    }
+}
